6eed27508531cc5a32ff104a317de72e638a4059
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2023-08-17 13:23:52 -0400

    working on node

--- src/Node.sol
+++ src/Node.sol
@@ -1,1 +1,20 @@
 // SPDX-License-Identifier: MIT
+pragma solidity =0.8.20;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Node is Ownable {
+    address parent;
+
+    mapping(uint256 => address) children;
+
+    constructor(address _parent) Ownable(msg.sender) {
+        parent = parent;
+    }
+
+    function getChild(uint256 _index) external view returns (address) {
+        return children[_index]
+    }
+
+    //function setChild()
+}
